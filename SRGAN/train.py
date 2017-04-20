ba0eb0fe7d43d65f6216ebb0a279df211b0c7cbf
inverse <534676033@qq.com>
2017-04-21 00:32:09 +0800

    bug fix

--- SRGAN/train.py
+++ SRGAN/train.py
@@ -1,7 +1,5 @@
 import os
-import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 import math
 import pdb
 
@@ -9,7 +7,8 @@ from srgan import SRGAN
 
 learning_rate = 1e-3
 batch_size = 32
-dataset = '../lfw/train'
+dataset = '/media/inverse/Document/eva'
+log_dir = './backup'
 
 def train():
     model = SRGAN(is_training=True)
@@ -17,29 +16,34 @@ def train():
     filenames = get_image_paths(dataset)
     count = len(filenames)
 
-    sess = tf.Session()
     queue = tf.RandomShuffleQueue(10000, batch_size, tf.string)
-    sess.run(queue.enqueue_many(tf.cast(filenames, tf.string)))
-
-    g_train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(model.g_loss, var_list=model.g_variables)
-    d_train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(model.d_loss, var_list=model.d_variables)
-
-    sess.run(tf.global_variables_initializer())
-
-    # Restore the SRGAN network
-    if tf.train.get_checkpoint_state('backup/'):
-        saver = tf.train.Saver()
-        saver.restore(sess, 'backup/latest')
-
-    loop = int(math.floor(count / batch_size))
-    for step in xrange(loop):
-        images = queue.dequeue_many(batch_size)
-        x_batch = get_images(images)
-        x_batch = tf.image.resize_images(x_batch, [224, 224], tf.image.ResizeMethod.BICUBIC)
+    init_queue = queue.enqueue_many((filenames,))
+    imagebatch = queue.dequeue_many(batch_size)
+    sess = tf.Session()
+    with sess.as_default():
+        init_queue.run()
         pdb.set_trace()
-        sess.run([g_train_op, d_train_op], feed_dict={model.x: x_batch})
-        if step % 50 == 0:
-            print('step:' % step)
+        g_train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(model.g_loss, var_list=model.g_variables)
+        d_train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(model.d_loss, var_list=model.d_variables)
+
+        sess.run(tf.global_variables_initializer())
+
+        # Restore the SRGAN network
+        summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
+        saver = tf.train.Saver()
+        summary = tf.summary.merge_all()
+
+        loop = int(math.floor(count / batch_size))
+        for step in xrange(loop):
+
+            x_batch = get_images(imagebatch.eval())
+            sess.run([g_train_op, d_train_op], feed_dict={model.x: x_batch})
+            summary_str = sess.run(summary)
+            summary_writer.add_summary(summary_str, step)
+            if step % 10 == 0:
+                checkpoint_file = os.path.join(log_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_file, global_step=step)
+                print('step:' % step)
 
 def get_image_paths(dataset):
     filenames = os.listdir(dataset)
@@ -48,7 +52,8 @@ def get_image_paths(dataset):
     return filenames
 
 def get_images(image_paths):
-    return tf.image.convert_image_dtype(tf.image.decode_jpeg(tf.read_file(image_paths), channels=3), dtype=tf.uint8)
+    batch = tf.image.convert_image_dtype(tf.image.decode_jpeg(tf.read_file(image_paths), channels=3), dtype=tf.uint8)
+    return tf.image.resize_images(batch, [224, 224], tf.image.ResizeMethod.BICUBIC)
 
 if __name__ == '__main__':
     train()
