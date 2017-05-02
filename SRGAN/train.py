7c49a59aa604ece9d6a94e7d8aa6a5e2dee09007
inverse <534676033@qq.com>
2017-05-02 16:11:39 +0800

    add new log

--- SRGAN/train.py
+++ SRGAN/train.py
@@ -2,6 +2,7 @@ import os
 import tensorflow as tf
 from srgan import SRGAN
 import time
+import resource
 
 initial_learning_rate = 0.0005
 epoch_times = 10
@@ -22,7 +23,7 @@ def train():
     filename_list = get_image_paths(dataset)
     count = len(filename_list)
     loop = int(count / batch_size)
-    queue = tf.RandomShuffleQueue(10 * count, batch_size, tf.string, shapes=())
+    queue = tf.RandomShuffleQueue(epoch_times * count, batch_size, tf.string, shapes=())
     init_queue = queue.enqueue_many((filename_list,))
     image = queue.dequeue()
 
@@ -46,25 +47,31 @@ def train():
         summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
         summary = tf.summary.merge_all()
         run_metadata = tf.RunMetadata()
+
+        print('Memory usage: {0}'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024))
+
         for epoch in xrange(epoch_times):
             init_queue.run()
-        while queue.size().eval() > batch_size:
-            start_time = time.time()
-            step = global_step.eval()
-            print('step: %d' % step)
-            x_batch = [get_image(image.eval()) for i in xrange(batch_size)]
-            summary_str, d_loss, g_loss = sess.run([summary, d_train_op, g_train_op], feed_dict={x: x_batch})
-            summary_writer.add_summary(summary_str, step)
+            while queue.size().eval() > batch_size:
+                start_time = time.time()
+                step = global_step.eval()
+                print('step: %d' % step)
+                x_batch = [get_image(image.eval()) for i in xrange(batch_size)]
+                print("read image cost: %ds" % (time.time() - start_time))
+                summary_str, d_loss, g_loss = sess.run([summary, d_train_op, g_train_op], feed_dict={x: x_batch})
+                summary_writer.add_summary(summary_str, step)
 
-            if global_step.eval() % 20 == 0:
-                checkpoint_file = os.path.join(model_dir, 'model.latest')
-                saver.save(sess, checkpoint_file)
+                if global_step.eval() % 20 == 0:
+                    checkpoint_file = os.path.join(model_dir, 'model.latest')
+                    saver.save(sess, checkpoint_file)
 
-            if global_step.eval() % 100 == 0:
-                summary_writer.add_run_metadata(run_metadata, 'step%03d' % step)
+                if global_step.eval() % 100 == 0:
+                    summary_writer.add_run_metadata(run_metadata, 'step%03d' % step)
 
-            sess.run(update_global_step)
-            print("step cost: %ds" % (time.time() - start_time))
+                sess.run(update_global_step)
+                print("step cost: %ds" % (time.time() - start_time))
+                print('Memory usage: {0}'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024))
+
         summary_writer.close()
 
     return d_train_op, g_train_op
